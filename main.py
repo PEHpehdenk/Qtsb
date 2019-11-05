8948eb6015233a3a7c57c29bb7c580739b5f6d43
roman <roma190505@yandex.ru>
2019-11-05 12:20:44 +0300

    начинаем что-то пытаться перенести в QtDesigner

--- main.py
+++ main.py
@@ -2,16 +2,34 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
-from PyQt5.QtWidgets import QLCDNumber, QLineEdit
+from PyQt5.QtWidgets import QLCDNumber, QLineEdit, QMainWindow
 from pygame import mixer  # Sound support
 import time
 import random
+
+import legacy
+from mainwindow import Ui_MainWindow
 
 field = [[[]] * 10] * 10
 field2 = [[[]] * 10] * 10
 
 # TODO: Убить Даниэля, кхм, не то...
-# TODO: Перенести сюда функцию legacy.randomShip
+# TODO: Перенести сюда функцию legacy.randomShip (нормальную!)
+def genBattleField():
+
+
 
 # TODO: QtDesigner клевая вещь, давайте ее использовать (ну как бы все сразу видно, что как смотрится)
 
+
+class GameWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = GameWindow()
+    window.show()
+    sys.exit(app.exec())
